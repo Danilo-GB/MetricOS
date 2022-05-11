d610314ae9793a8e7845f1b7c92ea7f21a4d1ec2
Danilo-GB <rubendanilo.garcia@gmail.com>
2022-05-10 22:04:07 -0500

    grid-layout added and working

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -4,22 +4,64 @@
  -->
 
 <template>
-	<div class="mb-24">
-		<div class="bg-white border-gray-3" style="height: calc(100vh - 89px); border: 1px solid">
-		</div>
-	</div>
+  <div class="mb-24">
+    <div class="border-gray-6">
+      <grid-layout
+        :layout.sync="layout"
+        :col-num="12"
+        :row-height="30"
+        :is-draggable="true"
+        :is-resizable="true"
+        :is-mirrored="false"
+        :margin="[10, 10]"
+      >
+        <grid-item
+          v-for="item in layout"
+          :x="item.x"
+          :y="item.y"
+          :w="item.w"
+          :h="item.h"
+          :i="item.i"
+          :key="item.i"
+        >
+          <!-- Widget 1 Card -->
+          <WidgetCounter
+            v-if="item.i === '1'"
+            title="hillow hillow"
+            :value="item.i"
+            prefix=""
+            suffix=""
+            icon='<a-icon type="appstore" theme="filled" />'
+          ></WidgetCounter>
+
+          <CardLineChart v-if="item.i === '2'"></CardLineChart>
+          <!-- / Widget 1 Card -->
+        </grid-item>
+      </grid-layout>
+    </div>
+  </div>
 </template>
 
 <script>
-
-	export default ({
-		data() {
-			return {
-			}
-		},
-	})
-
+import VueGridLayout from "vue-grid-layout";
+import WidgetCounter from "../components/Widgets/WidgetCounter";
+import CardLineChart from "../components/Cards/CardLineChart";
+export default {
+  components: {
+    GridLayout: VueGridLayout.GridLayout,
+    GridItem: VueGridLayout.GridItem,
+    WidgetCounter,
+    CardLineChart,
+  },
+  data() {
+    return {
+      layout: [
+        { x: 0, y: 0, w: 4, h: 2, i: "1" },
+        { x: 0, y: 2, w: 4, h: 8, i: "2" },
+      ],
+    };
+  },
+};
 </script>
 
-<style lang="scss">
-</style>
+<style lang="scss"></style>
